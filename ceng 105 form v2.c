2454d20828f8e17813274f47b475f7cb2f1b37de
Ahmet Selim BAL <149427829+Navatio@users.noreply.github.com>
2023-11-22 13:04:19 +0300

    added note

---
+++ ceng 105 form v2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main()
+{
+	int not,ort,id;
+	printf("enter your 3 digited id no:");
+	scanf("%d",&id);
+	printf("welcome %d",id);
+	printf("Enter your note");
+	scanf("%d", &not);
+	printf("Your note is: %d", not);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		return 0;
+}
+
+
