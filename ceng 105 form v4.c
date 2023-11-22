5ca2c753b684356c21db31e219b184aa6d10c508
Ahmet Selim BAL <149427829+Navatio@users.noreply.github.com>
2023-11-22 13:11:27 +0300

    added average note number and fixed the line problem

---
+++ ceng 105 form v4.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main()
+{
+	int not,ort,id,not2;
+	printf("enter your 3 digited id no\n:");
+	scanf("%d",&id);
+	printf("welcome %d",id);
+	printf("Enter your note\n");
+	scanf("%d", &not);
+	printf("Your note is: %d\n", not);
+	printf("enter your second notes:");
+	scanf("%d",&not2);
+	printf("your note 1 if &d and your note 2 is %d\n",not,not2);
+	printf("Your average  note is %d", (not+not2)/2);
+		
+		
+		
+		
+		
+		
+		
+		
+		return 0;
+}
+
+
