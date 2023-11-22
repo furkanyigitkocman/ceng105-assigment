76a7dc1a6471e875a8c03581765582e7974dcf5e
furkanyigitkocman <151139934+furkanyigitkocman@users.noreply.github.com>
2023-11-22 13:08:05 +0300

    addet second not and writed down both notes

---
+++ ceng 105 form v3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main()
+{
+	int not,ort,id,not2;
+	printf("enter your 3 digited id no:");
+	scanf("%d",&id);
+	printf("welcome %d",id);
+	printf("Enter your note");
+	scanf("%d", &not);
+	printf("Your note is: %d", not);
+	printf("enter your second notes:");
+	scanf("%d",&not2);
+	printf("your note 1 if &d and your note 2 is %d",not,not2);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		return 0;
+}
+
+
